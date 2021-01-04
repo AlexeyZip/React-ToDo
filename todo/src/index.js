e84bcac06f9a0598d61544702975162ad5046786
AlexeyZip <alexzipzap@gmail.com>
2021-01-04 18:05:49 +0200

    added app component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>
+const items = ['Learn React.js','Build awesome app']
+
+const TodoList = () => {
+ return (
+    <ul>
+    <li>{items[0]}</li>
+    <li>{items[1]}</li>
+  </ul>
+  )
+}
+
+const AppHeader = () => {
+return  <h1>My ToDo list</h1>
+}
 
 
-ReactDOM.render(el,document.getElementById('root')
-);
+
+const SearchPanel = () => {
+  const searchStyle = {fontSize: "20px"}
+  const searchText = 'What are you look for?'
+ return <input placeholder={searchText} style={searchStyle}></input>
+}
+
+const App = () => {
+ return (
+  <div>
+  <AppHeader/>
+  <SearchPanel/>
+ <TodoList/>
+</div>
+  )
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
